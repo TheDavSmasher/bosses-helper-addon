42676ebd4e111d29086df609a2d0faa342f2a941
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-09-29 12:29:48 -0600

    Changed return type to be exact

--- library/cutscene_meta.lua
+++ library/cutscene_meta.lua
@@ -13,5 +13,5 @@
 ---@alias ProxyResume fun(self: LuaCoroutineProxy, ...: any): boolean, string|LuaException|nil
 
 ---@alias LuaPreparer
----| fun(env: table, func: function): boolean
+---| fun(env: table, func: function): false
 ---| fun(env: table, func: function): function, ...
