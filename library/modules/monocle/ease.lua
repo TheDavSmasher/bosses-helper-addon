ef2181993ad5c5d9fb366cbc314459b0035aa8f0
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-09-29 15:00:59 -0600

    Moved Easers to Meta

--- library/modules/monocle/ease.lua
+++ library/modules/monocle/ease.lua
@@ -12,6 +12,40 @@ local c_Ease = {}
 
 --#endregion
 
+--#region Easers
+
+---@class c__Ease
+---@field Linear Ease.Easer
+---@field SineIn Ease.Easer
+---@field SineOut Ease.Easer
+---@field SineInOut Ease.Easer
+---@field QuadIn Ease.Easer
+---@field QuadOut Ease.Easer
+---@field QuadInOut Ease.Easer
+---@field CubeIn Ease.Easer
+---@field CubeOut Ease.Easer
+---@field CubeInOut Ease.Easer
+---@field QuintIn Ease.Easer
+---@field QuintOut Ease.Easer
+---@field QuintInOut Ease.Easer
+---@field ExpoIn Ease.Easer
+---@field ExpoOut Ease.Easer
+---@field ExpoInOut Ease.Easer
+---@field BackIn Ease.Easer
+---@field BackOut Ease.Easer
+---@field BackInOut Ease.Easer
+---@field BigBackIn Ease.Easer
+---@field BigBackOut Ease.Easer
+---@field BigBackInOut Ease.Easer
+---@field ElasticIn Ease.Easer
+---@field ElasticOut Ease.Easer
+---@field ElasticInOut Ease.Easer
+---@field BounceIn Ease.Easer
+---@field BounceOut Ease.Easer
+---@field BounceInOut Ease.Easer
+
+--#endregion
+
 ---Invert an [Ease.Easer](lua://Ease.Easer).
 ---@param easer Ease.Easer The [Ease.Easer](lua://Ease.Easer) to invert
 ---@return Ease.Easer inverse The inverted [Ease.Easer](lua://Ease.Easer)
