6f56a852f8bae34f4871d748d79ca0db0f736fa5
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-09-28 19:16:48 -0600

    Removed alias annotation

--- library/cutscene_meta.lua
+++ library/cutscene_meta.lua
@@ -15,7 +15,3 @@
 ---@alias LuaPreparer
 ---| fun(env: table, func: function): boolean
 ---| fun(env: table, func: function): function, ...
-
----@alias LuaGetData
----| fun(filename: string, data: InjectedData, preparer: LuaPreparer): (table, ...)
----| fun(filename: string, data: InjectedData, preparer: LuaPreparer): nil
