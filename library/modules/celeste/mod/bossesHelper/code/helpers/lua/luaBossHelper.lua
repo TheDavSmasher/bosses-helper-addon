dbf7772bcde31a9c79f3c9f4bc988cfb739dedea
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-10-13 17:34:49 -0600

    Changed to match new C# structure

--- library/modules/celeste/mod/bossesHelper/code/helpers/lua/luaBossHelper.lua
+++ library/modules/celeste/mod/bossesHelper/code/helpers/lua/luaBossHelper.lua
@@ -1,12 +1,15 @@
 ---@meta Celeste.Mod.BossesHelper.Code.Helpers.Lua.LuaBossHelper
 
 ---@class c__LuaBossHelper
----@field HelperFunctions string
 local LuaBossHelper = {}
 
 ---Get the string contents of the file.
 ---@param file string The file path.
 ---@return string contents
 function LuaBossHelper.GetFileContent(file) end
+
+---Get the string contents of the Helper Functions file.
+---@return string helperFuncs The Helper functions string contents.
+function LuaBossHelper.GetHelperFunctions() end
 
 return LuaBossHelper
